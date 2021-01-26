a0b4274bd8645fc800b285869f2bd78ea5c533fa
Jadoku <superpiras88@gmail.com>
2021-01-27 00:04:47 +0100

    coordinate, inizio di unità

--- Livello.py
+++ Livello.py
@@ -2,9 +2,23 @@ from Entita import Entita
 from Oggetto import Cat
 
 class Livello():
-    def __init__(self, lato: int = 21):
+    def __init__(self, lato: int = 31):
         # self.matrix=[[[0,0,0]]*lato for _ in range(lato)]
         self.lista_oggetti = []
+        self.lato = lato
+
+    def matrix(self):
+        """
+        popola la matrice con blocchi e pesi
+        :return: matrice con blocchi o pesi
+        """
+        matrix=[]
+        for i in range(self.lato):
+            r = []
+            for j in range(self.lato):
+                r.append(self.is_accessible(i,j))
+            matrix.append(r)
+        return matrix
 
     def get_coord(self, x, y, z: Cat = None):
         if z is None:
@@ -12,17 +26,32 @@ class Livello():
         else:
             return list(filter(lambda n: n.x == x and n.y == y and n.categoria == z, self.lista_oggetti))
 
-    def is_accessible(self, x, y): #se terreno bloccante risultato = 0, altrimenti risultato = modificatore del movimento
+    def is_accessible(self, x, y):
+        """
+        controlla se una casella è bloccante
+        :param x: coordinata X
+        :param y: coordinata Y
+        :return: 0 se bloccante, altrimenti modificatore di movimento.
+        """
         k = 0
         for i in self.get_coord(x,y):
             if i.block:
                 return 0
             else:
                 k += i.mod_movimento
-        return k
+        return max(k,0)
 
     def add_move(self,x,y,oggetto,add:bool=False,phase:bool=False):
-        if bool(self.is_accessible(x,y)) or phase:
+        """
+        aggiunge o muove un oggetto all'interno della griglia
+        :param x: destinazione x
+        :param y: destinazione y
+        :param oggetto: oggetto da muovere o aggiungere
+        :param add: se true, aggiunge un oggetto non presente sulla griglia
+        :param phase: ignora se il terreno è bloccante o meno
+        :return: true se è possibile, false se non è possibile
+        """
+        if self.check_bounds(x,y) and (bool(self.is_accessible(x,y)) or phase):
             oggetto.x = x
             oggetto.y = y
             if add:
@@ -39,4 +68,5 @@ class Livello():
                 self.add_move(oggetto.x,oggetto.y,i,True)
         return oggetto
 
-
+    def check_bounds(self,x,y):
+        return 0<=x<self.lato and 0<=y<self.lato
--- unita.py
+++ unita.py
@@ -1,4 +1,4 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from Entita import Entita
 from Oggetto import Cat
 
@@ -6,3 +6,13 @@ class Unita(Entita, ABC):
     def __init__(self):
         super().__init__()
         self.movimento = 1
+        self.ai = None
+
+    def muovi(self):
+        pass
+
+    def attacca(self):
+        pass
+
+    @abstractmethod
+    def esegui(self,target):
